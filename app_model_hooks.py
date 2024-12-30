86e2abbca9a561dce79a49df8f5b80851c37523d
carlosguerrasoria <guerrasoria@gmail.com>
2024-12-30 09:55:31 +0000

    Update app_model_hooks.py

--- app_model_hooks.py
+++ app_model_hooks.py
@@ -19,7 +19,7 @@ app.config["DEBUG"] = True
 # Enruta la landing page (endpoint /)
 @app.route("/", methods=["GET"])
 def hello():
-    return "Bienvenido a mi API del modelo advertising MODIFICADA lalala"
+    return "Bienvenido a mi API del modelo advertising MODIFICADA lalala 2"
 
 
 # Enruta la funcion al endpoint /api/v1/predict
